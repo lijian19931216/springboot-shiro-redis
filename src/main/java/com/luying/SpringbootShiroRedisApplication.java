8bc453b384400fe483e58373ff1507dce602534d
lijian123123 <928229131@qq.com>
2018-08-10 00:04:00 +0800

    Initial commit

---
+++ src/main/java/com/luying/SpringbootShiroRedisApplication.java
@@ -0,0 +1,12 @@
+package com.luying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroRedisApplication.class, args);
+    }
+}
